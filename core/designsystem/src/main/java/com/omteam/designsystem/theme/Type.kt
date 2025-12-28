3011645fd661c979fe4ef219e2c5e48752512a62
ask0908 <askdfjaslk@naver.com>
2025-12-28 21:34:45 +0900

    project: paperlogy 폰트 추가

--- core/designsystem/src/main/java/com/omteam/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/omteam/designsystem/theme/Type.kt
@@ -8,35 +8,70 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.omteam.omt.core.designsystem.R
 
-val PretendardFont = FontFamily(
-    Font(R.font.pretendard_thin, FontWeight.Thin),  // 100
-    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),// 200
-    Font(R.font.pretendard_light, FontWeight.Light),          // 300
-    Font(R.font.pretendard_regular, FontWeight.Normal),       // 400
-    Font(R.font.pretendard_medium, FontWeight.Medium),        // 500
-    Font(R.font.pretendard_semibold, FontWeight.SemiBold),    // 600
-    Font(R.font.pretendard_bold, FontWeight.Bold),            // 700
-    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),  // 800
-    Font(R.font.pretendard_black, FontWeight.Black)           // 900
+// 메인 폰트
+val PaperlogyFont = FontFamily(
+    Font(R.font.paperlogy_thin, FontWeight.Thin),           // 100
+    Font(R.font.paperlogy_extralight, FontWeight.ExtraLight),   // 200
+    Font(R.font.paperlogy_light, FontWeight.Light),             // 300
+    Font(R.font.paperlogy_regular, FontWeight.Normal),          // 400
+    Font(R.font.paperlogy_medium, FontWeight.Medium),           // 500
+    Font(R.font.paperlogy_semibold, FontWeight.SemiBold),       // 600
+    Font(R.font.paperlogy_bold, FontWeight.Bold),               // 700
+    Font(R.font.paperlogy_extrabold, FontWeight.ExtraBold),     // 800
+    Font(R.font.paperlogy_black, FontWeight.Black)              // 900
 )
 
+// 서브 폰트
+val PretendardFont = FontFamily(
+    Font(R.font.pretendard_thin, FontWeight.Thin),           // 100
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),  // 200
+    Font(R.font.pretendard_light, FontWeight.Light),            // 300
+    Font(R.font.pretendard_regular, FontWeight.Normal),         // 400
+    Font(R.font.pretendard_medium, FontWeight.Medium),          // 500
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),      // 600
+    Font(R.font.pretendard_bold, FontWeight.Bold),              // 700
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),    // 800
+    Font(R.font.pretendard_black, FontWeight.Black)             // 900
+)
+
+/**
+ * MaterialTheme.typography 통해 접근
+ *
+ * ```kotlin
+ * Text(
+ *     text = "메인 폰트",
+ *     style = MaterialTheme.typography.bodyLarge  // paperlogy 적용됨
+ * )
+ * ```
+ *
+ * 직접 명시해서 사용
+ *
+ * ```kotlin
+ * Text(
+ *     text = "서브 폰트",
+ *     fontFamily = PretendardFont,
+ *     fontWeight = FontWeight.Medium
+ * )
+ * ```
+ *
+ */
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = PretendardFont,
+        fontFamily = PaperlogyFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = PretendardFont,
+        fontFamily = PaperlogyFont,
         fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = PretendardFont,
+        fontFamily = PaperlogyFont,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
