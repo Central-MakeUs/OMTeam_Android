a8d6c3c8a574b90c4627a87e02877d4e486538c4
ask0908 <askdfjaslk@naver.com>
2026-01-09 19:00:11 +0900

    refactor: 앱 첫 시작이 LoginScreen으로 표시되는지 확인

--- feature/login/src/main/java/com/omteam/login/LoginScreen.kt
+++ feature/login/src/main/java/com/omteam/login/LoginScreen.kt
@@ -1,5 +1,8 @@
 package com.omteam.login
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -7,5 +10,9 @@ import androidx.compose.ui.Modifier
 fun LoginScreen(
     modifier: Modifier = Modifier,
 ) {
-    //
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text("로그인 화면")
+    }
 }
