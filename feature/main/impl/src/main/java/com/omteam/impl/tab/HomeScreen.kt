57bb379b6789d2ff30fd9a02a8b775a1f0d9aece
ask0908 <askdfjaslk@naver.com>
2026-01-30 20:34:17 +0900

    refactor: MainViewModel 내부 로직 분리

--- feature/main/impl/src/main/java/com/omteam/impl/tab/HomeScreen.kt
+++ feature/main/impl/src/main/java/com/omteam/impl/tab/HomeScreen.kt
@@ -40,27 +40,21 @@ import com.omteam.impl.viewmodel.AppleStatus
 import com.omteam.impl.viewmodel.CharacterUiState
 import com.omteam.impl.viewmodel.DailyAppleData
 import com.omteam.impl.viewmodel.DailyMissionUiState
-import com.omteam.impl.viewmodel.MainViewModel
+import com.omteam.impl.viewmodel.HomeViewModel
 import com.omteam.omt.core.designsystem.R
-import timber.log.Timber
 import java.time.LocalDate
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    viewModel: MainViewModel = hiltViewModel()
+    homeViewModel: HomeViewModel = hiltViewModel()
 ) {
-    val dailyMissionUiState by viewModel.dailyMissionUiState.collectAsStateWithLifecycle()
-    val characterUiState by viewModel.characterUiState.collectAsStateWithLifecycle()
-    val weeklyReportUiState by viewModel.weeklyReportUiState.collectAsStateWithLifecycle()
-    Timber.e("[HomeScreen] 주간 레포트 상태 : $weeklyReportUiState")
+    val dailyMissionUiState by homeViewModel.dailyMissionUiState.collectAsStateWithLifecycle()
+    val characterUiState by homeViewModel.characterUiState.collectAsStateWithLifecycle()
 
     LaunchedEffect(Unit) {
-        viewModel.apply {
-            fetchDailyMissionStatus()
-            fetchCharacterInfo()
-            fetchWeeklyReport()
-        }
+        homeViewModel.fetchDailyMissionStatus()
+        homeViewModel.fetchCharacterInfo()
     }
 
     Column(
@@ -92,7 +86,7 @@ fun HomeScreen(
             // 오늘이 속한 주를 표시
             // 14~20일 중 하나에 속하면 14~20 표시, 21일 되면 21~27 표시
             WeeklyAppleView(
-                weekDays = viewModel.getCurrentWeekDays(),
+                weekDays = homeViewModel.getCurrentWeekDays(),
                 modifier = Modifier.fillMaxWidth()
             )
 
