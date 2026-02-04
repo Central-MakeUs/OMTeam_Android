6b35c11d9489a6b5f52f77d28aaba55fbae8f340
ask0908 <askdfjaslk@naver.com>
2026-02-04 22:44:14 +0900

    refactor: 각 정보 수정 화면 구조 변경

--- feature/main/impl/src/main/java/com/omteam/impl/tab/mypage/EditFavoriteExerciseScreen.kt
+++ feature/main/impl/src/main/java/com/omteam/impl/tab/mypage/EditFavoriteExerciseScreen.kt
@@ -1,10 +1,16 @@
 package com.omteam.impl.tab.mypage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.omteam.designsystem.foundation.dp20
 import com.omteam.designsystem.theme.OMTeamTheme
+import com.omteam.designsystem.theme.White
 import com.omteam.impl.component.SubScreenHeader
 import com.omteam.omt.core.designsystem.R
 
@@ -16,11 +22,18 @@ fun EditFavoriteExerciseScreen(
     modifier: Modifier = Modifier,
     onBackClick: () -> Unit = {}
 ) {
-    // 상단 헤더
-    SubScreenHeader(
-        title = stringResource(R.string.edit_my_info_title),
-        onBackClick = onBackClick
-    )
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(White)
+            .padding(dp20)
+    ) {
+        // 상단 헤더
+        SubScreenHeader(
+            title = stringResource(R.string.edit_my_info_title),
+            onBackClick = onBackClick
+        )
+    }
 }
 
 @Preview
