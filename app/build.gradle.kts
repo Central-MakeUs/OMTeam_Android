2de449dd409914a4daa0de72f244f487c5c3670a
ask0908 <askdfjaslk@naver.com>
2026-02-12 21:38:10 +0900

    project: 앱 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.omteam.omt"
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
         
         buildConfigField(
